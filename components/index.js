9012a05acf09271a9be22e19a87a2e9447f172bc
De Boeck Stef <91065663+pgm-stefdebo3@users.noreply.github.com>
2024-02-25 17:59:52 +0100

    Updated typo

--- components/index.js
+++ components/index.js
@@ -4,6 +4,6 @@ import NavBar from "./Navbar";
 import Quote from "./Quote";
 import TopButton from "./Topbutton";
 import Contact from "./Contact/Contact";
-import SkillList from "./skills/SkillList";
+import SkillList from "./Skills/SkillList";
 
 export {Sphere, SkillList, Card, NavBar, Quote, TopButton,  Contact};
